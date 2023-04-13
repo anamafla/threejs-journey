7ed6c8fe4a6191427f18767aff727305f78b5d6f
Ana Mafla <anamafla@hotmail.com>
2023-04-13 10:54:15 -0500

    Transform objects

--- lessons-chapter-one/src/script.js
+++ lessons-chapter-one/src/script.js
@@ -4,10 +4,31 @@ import * as THREE from 'three';
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+scene.add(group)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+
+group.add(cube1)
+cube1.position.x = -1.5
+
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+
+group.add(cube2)
+cube2.position.x = 0
+
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
+
+group.add(cube3)
+cube3.position.x = 1.5
 
 // Sizes
 const sizes = {
